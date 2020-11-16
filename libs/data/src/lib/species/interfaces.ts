075dc461b8e168b322911d783c54f9058e398c9b
Michael Lage <lage.michael@gmail.com>
2020-11-16 09:26:13 -0700

    environmentId foreign key added to Species <-> Plant Stages table
    - species has many plant stages, and for each stage, can have its own
      respective enviornment to specify ideal conditions
    - this change allows us to reuse plant stages ie Sprouting, across
      various species BUT with different ideal env conditions for each
    - therefore we'll be able to group plants by common plant stages, even
      if their environments are different

--- libs/data/src/lib/species/interfaces.ts
+++ libs/data/src/lib/species/interfaces.ts
@@ -1,4 +1,4 @@
-import { Species, PlantStage, BaseDbModel } from './../generated';
+import { Species, PlantStage, BaseDbModel, Environment } from './../generated';
 
 /**
  * These model the tables in the TBI database
@@ -10,8 +10,11 @@ import { Species, PlantStage, BaseDbModel } from './../generated';
 // Many:Many Table
 //
 
-// Relationship Table between Species <-> Life-Cycles
+// Relationship Table between Species <-> Plant Stages / Environements
+// Species has plant stages and for anyone of those stages, it can have a specific environment with ideal conditions for plant growth
+//  adding environentId here makes it possible to share plant stages across species, but with varying ideal environmental conditions
 export interface DBSpeciesPlantStages extends BaseDbModel {
   speciesId: Species['id'],
-  plantStageId: PlantStage['id']
+  plantStageId: PlantStage['id'],
+  environmentId: Environment['id']
 }
